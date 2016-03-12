052a8b9c77bcf2c18841444673eed6a65f6b11ef
TechFxs <techfxs@gmail.com>
2016-03-12 06:24:13 +0530

    cleanedup unnecessary files

--- public/js/app.js
+++ public/js/app.js
@@ -8,111 +8,51 @@
  *
  * Main module of the application.
  */
-// angular
-//   .module('ufaasApp', [
-//     'ngAnimate',
-//     'ngAria',
-//     'ngCookies',
-//     'ngResource',
-//     'ui.router',
-//     'ngSanitize',
-//     'ngTouch'
-//   ])
-//   .config(function ($routeProvider) {
-//     $routeProvider
-//       .when('/', {
-//         templateUrl: 'templates/main.html',
-//         controller: 'MainCtrl',
-//         controllerAs: 'main'
-//       })
-//       .when('/about', {
-//         templateUrl: 'templates/about.html',
-//         controller: 'AboutCtrl',
-//         controllerAs: 'about'
-//       })
-//       .otherwise({
-//         redirectTo: '/'
-//       });
-//   });
-
-
-  //.config(function($stateProvider, $urlRouterProvider) {
-  // //
-  // // For any unmatched url, redirect to /state1
-  // $urlRouterProvider.otherwise("/");
-  // //
-  // // Now set up the states
-  // $stateProvider
-  //   .state('state1', {
-  //     url: "/",
-  //     templateUrl: 'templates/main.html',
-  //     controller: 'MainCtrl',
-  //     controllerAs: 'main'
-  //   })
-
-  //   });
 
 angular
-  .module('ufaasApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngResource',
-    'ui.router',
-    'ngSanitize',
-    'ngTouch'
-   ]).config(function($stateProvider, $urlRouterProvider){
- 
-    $urlRouterProvider
-    .otherwise('/');
-    
+    .module('ufaasApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngResource',
+        'ui.router',
+        'ngSanitize',
+        'ngTouch'
+    ]).config(function($stateProvider, $urlRouterProvider) {
 
- $stateProvider
-    .state('root',{        
-         abstract: true,
-         url : "",
-         views: {
-            'header': {
-                templateUrl: 'views/common/header.html'
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+        .state('root', {
+            abstract: true,
+            url: "",
+            views: {
+                'header': {
+                    templateUrl: 'views/nonauth/common/header.html'
+                },
+                'footer': {
+                    templateUrl: 'views/nonauth/common/footer.html'
+                }
+            }
+          })
+        .state('root.home', {
+            url: '/',
+            views: {
+                'content@': {
+                    templateUrl: 'views/nonauth/home.html',
+                    controller: "HomeCtrl as homeCtrl"
+                }
             },
-            'footer': {
-                templateUrl: 'views/common/footer.html'                
-            }
-        }
-    })
-
-    /*Page id : CDS1*/
-
-    .state('root.home',{
-        url: '/',
-        views: {           
-            'content@': {
-                templateUrl: 'views/home.html',
-                controller : "MainCtrl as main"                
-            }
-             
-        },
-        secured : false
-    })
-    .state('root.signin',{
-        url: '/signin',
-        views: {           
-            'content@': {
-                templateUrl: 'views/nonauth/signin.html',
-                controller : "signinController as signinCtrl"
-            }
-        },
-        secured : false
-    })
-    .state('root.knowyourmp',{
-        url: '/knowyourmp',
-        views: {           
-            'content@': {
-                templateUrl: 'views/nonauth/knowyourmp.html'
-            }
-        },
-        secured : false
-    })
-
-
-  });
+            secured: false
+        })
+        // .state('root.signin', {
+        //     url: '/signin',
+        //     views: {
+        //         'content@': {
+        //             templateUrl: 'views/nonauth/signin.html',
+        //             controller: "signinController as signinCtrl"
+        //         }
+        //     },
+        //     secured: false
+        // })
+    });
--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -8,7 +8,7 @@
  * Controller of the ufaasApp
  */
 angular.module('ufaasApp')
-  .controller('MainCtrl', function () {
+  .controller('HomeCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
