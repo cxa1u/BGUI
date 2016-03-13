5804514d2d6134d3500a98caf553a98d04c2a691
TechFxs <techfxs@gmail.com>
2016-03-13 17:54:59 +0530

    updated map marker

--- public/js/map-access.js
+++ public/js/map-access.js
@@ -2,19 +2,81 @@
 
 var map;
 
+var bluishStyle = [{
+    stylers: [{
+        hue: "#4C5F2A"
+    }, {
+        saturation: -5
+    }, {
+        lightness: -40
+    }]
+}, {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{
+        lightness: 100
+    }, {
+        visibility: "simplified"
+    }]
+}, {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{
+        hue: "#0000FF"
+    }, {
+        saturation: -40
+    }]
+}, {
+    featureType: "administrative.neighborhood",
+    elementType: "labels.text.stroke",
+    stylers: [{
+        color: "#E80000"
+    }, {
+        weight: 1
+    }]
+}, {
+    featureType: "road",
+    elementType: "labels.text",
+    stylers: [{
+        // visibility: "off"
+    }]
+}, {
+    featureType: "road.highway",
+    elementType: "geometry.fill",
+    stylers: [{
+        color: "#7C512E"
+    }, {
+        weight: 2
+    }]
+}];
+
 function initMap() {
     console.log(google.maps)
 
     google.maps.visualRefresh = true;
 
+    var bluishStyledMap = new google.maps.StyledMapType(bluishStyle, {
+        //name: "BITEGATE MAPS"
+    });
+
+
     var mapOptions = {
-        center: new google.maps.LatLng(39.9078, 32.8252),
-        zoom: 15,
+        backgroundColor: "#CADFAA",
+        center: new google.maps.LatLng(51.508742,-0.120850),
+        zoom: 10,
+        mapTypeControl: false,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP]
+        },
+        styles: [bluishStyledMap],
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     var mapElement = document.getElementById('mapContainer');
     map = new google.maps.Map(mapElement, mapOptions);
+
+    map.mapTypes.set('new_bluish_style', bluishStyledMap);
+    map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -22,10 +84,27 @@ function initMap() {
             var lng = position.coords.longitude;
             //Creating LatLng object with latitude and
             //longitude.
-            var userLocation = new google.maps.LatLng(lat, lng);
-            map.setCenter(userLocation);
-            map.setZoom(15);
-            //map.setTilt(180);
+            var userLocation = new google.maps.LatLng(51.508742,-0.120850);
+           // map.setCenter(userLocation);
+           // map.setZoom(15);
+            var posObjArray = [];
+            posObjArray[0] = new google.maps.LatLng(51.508742,-0.120850);
+            generateMarkers(posObjArray, map);
         });
+    }
+}
+
+
+function generateMarkers(posObjArray, mapObj) {
+
+    if (posObjArray.length && !$.isEmptyObject(mapObj)) {
+        var i;
+        for (i = 0; i < posObjArray.length; i++) {
+            var marker = new google.maps.Marker({
+                position: posObjArray[i],
+                //animation: google.maps.Animation.BOUNCE
+            });
+            marker.setMap(mapObj);
+        }
     }
 }
