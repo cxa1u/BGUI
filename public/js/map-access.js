c92c25f3359aa1b7fca438fefc2969fc35ebd1e8
TechFxs <techfxs@gmail.com>
2016-03-13 20:25:51 +0530

    updated marker icon

--- public/js/map-access.js
+++ public/js/map-access.js
@@ -1,18 +1,21 @@
 //@@ Maps
 
 var map;
-var posObjArray = [];
+var posObjArray = [{},{},{},{}];
 
 function initMap() {
 
     google.maps.visualRefresh = true;
 
     // Default markers
-    posObjArray[0] = new google.maps.LatLng(37.3831663,-121.9972578);
-    posObjArray[1] = new google.maps.LatLng(37.3817151,-121.9975511);
-    posObjArray[2] = new google.maps.LatLng(37.3317121,-121.9978455);
-    posObjArray[3] = new google.maps.LatLng(37.3891982,-121.9858969);
-
+    posObjArray[0].position = new google.maps.LatLng(37.3831663,-121.9972578);
+    posObjArray[0].icon = "images/marker_green.png";
+    posObjArray[1].position = new google.maps.LatLng(37.3817151,-121.9975511);
+    posObjArray[1].icon = "images/marker_green.png";
+    posObjArray[2].position = new google.maps.LatLng(37.3317121,-121.9978455);
+    posObjArray[2].icon = "images/marker_green.png";
+    posObjArray[3].position = new google.maps.LatLng(37.3891982,-121.9858969);
+    posObjArray[3].icon = "images/marker_green.png";
 
     var mapOptions = {
         backgroundColor: "#CADFAA",
@@ -37,7 +40,8 @@ function generateMarkers(posObjArray, mapObj) {
         var i;
         for (i = 0; i < posObjArray.length; i++) {
             var marker = new google.maps.Marker({
-                position: posObjArray[i],
+                position: posObjArray[i].position,
+                icon : posObjArray[i].icon,
                 //animation: google.maps.Animation.BOUNCE
             });
             marker.setMap(mapObj);
